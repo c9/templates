c0a7742170a15bf71280424d77289881596a4b87
Fabian Jakobs <fabian@c9.io>
2014-03-31 14:56:04 +0000

    use latest ubnutu and refactor docker bash scripts

---
+++ workspace/files/cloud9.sh
@@ -0,0 +1,22 @@
+export C9_FULLNAME=
+export C9_EMAIL=
+export C9_USER=
+export C9_PROJECT=
+export C9_PID=
+export C9_UID=
+
+export C9_PORT=8080
+export C9_IP=127.0.0.1
+export C9_SHARED=/mnt/shared
+
+export PATH=/mnt/shared/bin:$HOME/.nada-nix-profile/bin:$HOME/workspace/node_modules/.bin:$HOME/bin:/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/local/go:/bin/mnt/shared/sbin
+export HGUSER=$C9_FULLNAME
+export EMAIL=$C9_EMAIL
+export PORT=$C9_PORT
+export IP=$C9_IP
+export PYTHONPATH=$PYTHONPATH:$HOME/lib/python/site-packages
+export GEM_PATH=$GEM_PATH:/mnt/shared/lib/ruby
+
+export GOROOT=/usr/local/go
+export METEOR_IP=$IP
+export METEOR_PORT=$PORT
