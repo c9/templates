964e5814ea8e1cbfc6be39898a590430bcde316d
Lennart Kats <lennart@lclnet.nl>
2014-04-09 14:41:22 +0000

    Add migration script for nada-nix
    Fixes #1619

---
+++ workspace/files/nada-nix-compat.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+PROFILE=~/.nada-nix-profile
+
+installedPackages() {
+  if [ -e $PROFILE/list ]; then
+    cat $PROFILE/list
+    return
+  fi
+  TEMPFILE=`tempfile`
+  find $PROFILE -type l | while read F; do
+    readlink $F
+  done | sed -E 's/^[^-]*-([a-zA-Z][^-/]*(-[^-/]*)*).*/\1/' | sort | uniq | tee $TEMPFILE
+  mv -f $TEMPFILE $PROFILE/list
+}
+
+if [ ! -e $PROFILE ] || [ -e $PROFILE/done ]; then
+  return
+fi
+
+INSTALLED=`installedPackages`
+
+echo "Hi there! Welcome to your newly updated Cloud9 workspace!"
+echo
+echo "We noticed you installed the following packages on your old workspace:"
+echo
+for I in $INSTALLED; do echo " - $I"; done
+echo
+echo "You may have to install these again on your new workspace."
+echo "You can use the following commands to install packages:"
+echo
+echo "- sudo apt-get install PACKAGE   # installs a package"
+echo "- sudo apt-cache search TEXT     # search for a package"
+echo
+echo "This message will self-destruct once you run one of the commands above ;)"
+
+apt_get() {
+    echo done > $PROFILE/done &> /dev/null
+    command apt-get "$@"
+}
+apt_cache() {
+    echo done > $PROFILE/done &> /dev/null
+    command apt-cache "$@"
+}
+export -f apt_get apt_cache
+alias apt-get=apt_get
+alias apt-cache=apt_cache
