f4aac9963d7a678435e3cce3b3d1442ef3532df1
Lennart kats <lennart@c9.io>
2014-09-25 09:17:51 +0000

    Fix shellshock bug in existing workspaces

--- workspace/files/etc/profile.d/cloud9.sh
+++ workspace/files/etc/profile.d/cloud9.sh
@@ -51,6 +51,11 @@ for S in /mnt/shared/profile.d/*; do
     [ -e $S ] && . $S
 done
 
+if X='() { :; }; echo Vulnerable' bash -c pwd 2>/dev/null | grep -q Vulnerable; then
+    ( sudo apt-get update &>/dev/null &&
+      sudo apt-get install bash &>/dev/null) &
+fi
+
 _xdgopen() {
     if [ -e "$@" ]; then
         c9 "$@"
