104eb4902b281d8ca1d74e09f95d7486790789d8
Fabian Jakobs <fabian@c9.io>
2015-11-19 14:54:00 +0000

    some ws names conflict with the names of existing Python modules

--- ws-python/files/init.sh
+++ ws-python/files/init.sh
@@ -4,10 +4,12 @@ set -e
 
 cd ~/workspace
 C9_PROJECT_SANITIZED=`echo $C9_PROJECT | sed 's/[^A-Za-z_0-9]/_/g' | sed -E 's/^[^A-Za-z_]/_/'`
-if [ "$C9_PROJECT_SANITIZED" == "django" ]; then
-    C9_PROJECT_SANITIZED=django_project
+
+# some names conflict with the names of existing Python modules
+if ! django-admin.py startproject $C9_PROJECT_SANITIZED . &> /dev/null ; then
+    C9_PROJECT_SANITIZED=c9_$C9_PROJECT_SANITIZED
+    django-admin.py startproject $C9_PROJECT_SANITIZED .
 fi
 
-django-admin.py startproject $C9_PROJECT_SANITIZED .
 sed -i "s/HOSTNAME/$C9_HOSTNAME/g" /home/ubuntu/workspace/README.md
 sed -i "/ALLOWED_HOSTS/a SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')" /home/ubuntu/workspace/$C9_PROJECT_SANITIZED/settings.py
