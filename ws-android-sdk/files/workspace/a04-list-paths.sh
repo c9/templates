b662b103dc6314bdb35bf239b0729cac6ce8cad3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-21 20:24:52 -0800

    Create a04-list-paths.sh

---
+++ ws-android-sdk/files/workspace/a04-list-paths.sh
@@ -0,0 +1,20 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a04-list-paths.sh
+#  best in cloud 9 to just right click this file and select run
+
+
+
+echo "The Android SDK home is ANDROID_HOME"
+echo "$ANDROID_HOME"
+echo ""
+
+echo "The Android ndk home is"
+echo "/usr/local/android-ndk-r10e"
+echo ""
+
+echo "The path variable is"
+echo "$PATH"
+echo ""
+
