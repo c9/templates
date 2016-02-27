be55483d7880c743291dc734a9180a90149e7fe4
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-27 07:34:54 -0800

    Update profile.d-android.sh

--- ws-android/files/profile.d-android.sh
+++ ws-android/files/profile.d-android.sh
@@ -1,7 +1,10 @@
 #!/bin/bash  
 
+echo "Setting up environment variables"
 export ANDROID_HOME=/usr/local/android-sdk-linux
 export ANDROID_SDK_HOME=/usr/local/android-sdk-linux
 export GRADLE_HOME=/usr/local/gradle-2.5
 
 export PATH=$PATH:$ANDROID_HOME:$ANDROID_SDK_HOME/tools:$ANDROID_SDK_HOME/platform-tools:$GRADLE_HOME/bin
+
+echo "Environment setup"
