21d74c38ec8fae15f3dab17386975db0e9b3cde4
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-26 17:22:38 -0800

    Update profile.d-android.sh

--- ws-android/files/profile.d-android.sh
+++ ws-android/files/profile.d-android.sh
@@ -3,4 +3,4 @@ export ANDROID_SDK_HOME=/usr/local/android-sdk-linux
 export ANDROID_NDK_HOME=/usr/local/android-ndk-r10e
 export GRADLE_HOME=/usr/local/gradle-2.5
 export NDK_PROJECT_PATH=/home/ubuntu/workspace
-export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/tools:$ANDROID_SDK_HOME/platform-tools:$ANDROID_NDK_HOME:$GRADLE_HOME/bin
+export PATH=$PATH:$ANDROID_HOME:$ANDROID_SDK_HOME/tools:$ANDROID_SDK_HOME/platform-tools:$ANDROID_NDK_HOME:$GRADLE_HOME/bin
