55b91bf908f97cff85384a11366bfe47d25da40d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-27 23:42:09 -0800

    Update a00-auto-ant-gradle.sh

--- ws-android/files/workspace/a00-auto-ant-gradle.sh
+++ ws-android/files/workspace/a00-auto-ant-gradle.sh
@@ -20,7 +20,7 @@ service apache2 start
 
 echo "Lets make an android ant apk file"
 
-read=HelloAnt
+wow4=helloAnt
 
 ls -l /home/ubuntu/workspace/android-sdk-linux/platforms
 
@@ -30,7 +30,7 @@ echo "Type 20 for --target android-20    Android 4.4W.2 KitKat, type 20"
 echo "Type 22 for --target android-22    Android 5.1.1  Lollipop, type 22"
 echo "Type 23 for --target android-23    Android 6.0    Marshmallow, type 23"
 
-read=20
+myNum=20
 
 
 #http://socialcompare.com/en/comparison/android-versions-comparison
@@ -62,7 +62,7 @@ echo "Look for you new android $wow4/bin $wow4-debug.apk"
 echo "right-click run index.html, then preview-preview running application to view webpage with .apk"
 
 echo "Finally lets make a Gradle Android apk and show the website to download all three"
-read=HelloGradle
+wow4=helloGradle
 
 
 ls -l /home/ubuntu/workspace/android-sdk-linux/platforms
@@ -73,7 +73,7 @@ echo "Type 20 for --target android-20    Android 4.4W.2 KitKat, type 20"
 echo "Type 22 for --target android-22    Android 5.1.1  Lollipop, type 22"
 echo "Type 23 for --target android-23    Android 6.0    Marshmallow, type 23"
 
-read=20
+myNum=20
 
 
 #Good site to check
