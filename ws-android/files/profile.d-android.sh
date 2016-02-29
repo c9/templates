68cfa34741f39b081cf638550e2c47794d1cca4d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-29 14:14:21 -0800

    Update profile.d-android.sh

--- ws-android/files/profile.d-android.sh
+++ ws-android/files/profile.d-android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Setting up environment variables'
+echo 'Setting up environment variables etc..'
 JAVA_HOME /usr/lib/jvm/java-7-openjdk-amd64
 PATH $PATH:$JAVA_HOME/bin
 
@@ -12,5 +12,17 @@ PATH $PATH:$ANDROID_SDK_HOME/platform-tools
 
 GRADLE_HOME /usr/local/gradle-2.9
 PATH $PATH:$GRADLE_HOME/bin
+
+# now make some symlinks, fake folders
+
+echo "Making a symlink to the Android-SDK"
+ln -s /usr/local/android-sdk-linux /home/ubuntu/workspace/android-SDK-link
+
+echo "Making a symlink to Gradle"
+ln -s /usr/local/gradle-2.9 /home/ubuntu/workspace/Gradle-link
+
+echo "Making a symlink to start-here"
+ln -s /homeubuntu/start-here /home/ubuntu/workspace/start-here-link
+
 
 echo 'Environment variables and path complete'
