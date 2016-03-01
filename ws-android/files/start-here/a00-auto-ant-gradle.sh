e61d26f5002462d2093360ad72e928d6431f94a3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-29 21:56:03 -0800

    Update a00-auto-ant-gradle.sh

--- ws-android/files/start-here/a00-auto-ant-gradle.sh
+++ ws-android/files/start-here/a00-auto-ant-gradle.sh
@@ -5,28 +5,9 @@
 #  best in cloud 9 to just right click this file and select run
 
 
-
-
-
-#echo "Activating apache2 webserver"
-
-#service apache2 start
-
-
-
-
-
 echo "Lets make an android ant apk file"
 
 wow4=helloAnt
-
-ls -l /usr/local/android-sdk-linux/platforms
-
-echo "Type the number of the target your Android app is for"
-echo "See above folders of installed platforms"
-echo "Type 20 for --target android-20    Android 4.4W.2 KitKat, type 20"
-echo "Type 22 for --target android-22    Android 5.1.1  Lollipop, type 22"
-echo "Type 23 for --target android-23    Android 6.0    Marshmallow, type 23"
 
 myNum=20
 
@@ -62,36 +43,22 @@ echo "Look for your new android $wow4/bin $wow4-debug.apk"
 echo "right-click run index.html, then preview-preview running application to view webpage with .apk"
 
 
+
+
+
+
+
+
 #Now lets make an Android .apk using Gradle
 
 #http://socialcompare.com/en/comparison/android-versions-comparison
 
 cd /home/ubuntu/workspace
 
-
-# Make the following a full path to where your index.html file is /home/ubuntu/workspace/www/index.html
-
-printf "\n\n<a href='$wow4/bin/$wow4-debug.apk'>$wow4/bin/$wow4-debug.apk</a><br>"  >> /home/ubuntu/workspace/www/index.html
-
-ls -l
-
-echo "Look for you new android $wow4/bin $wow4-debug.apk"
-echo "right-click run index.html, then preview-preview running application to view webpage with .apk"
-
 echo "Finally lets make a Gradle Android apk and show the website to download all three"
 wow4=helloGradle
 
-
-ls -l /usr/local/android-sdk-linux/platforms
-
-echo "Type the number of the target your Android app is for"
-echo "See above folders of installed platforms"
-echo "Type 20 for --target android-20    Android 4.4W.2 KitKat, type 20"
-echo "Type 22 for --target android-22    Android 5.1.1  Lollipop, type 22"
-echo "Type 23 for --target android-23    Android 6.0    Marshmallow, type 23"
-
 myNum=20
-
 
 #Good site to check
 #http://socialcompare.com/en/comparison/android-versions-comparison
@@ -107,15 +74,9 @@ android create project \
 --gradle --gradle-version 0.11.+
 
 
-
-
 cd /home/ubuntu/workspace/$wow4
 
-
-chmod a+x gradlew
-
 sudo ./gradlew assembleDebug
-
 
 # Make the following a full path to where your index.html file is /home/ubuntu/workspace/www/index.html
 
