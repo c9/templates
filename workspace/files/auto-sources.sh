f70bc05ffd82d6989c24363f9c50b9084b25b58d
Justin Dray <justin@dray.be>
2016-03-02 14:47:55 +1000

    Change to master ubuntu mirrors by default not their terrible auto-mirror stuff

--- workspace/files/auto-sources.sh
+++ workspace/files/auto-sources.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-DEFAULT="deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse"
+DEFAULT="deb http://archive.ubuntu.com/ubuntu/ trusty main restricted
+deb-src http://archive.ubuntu.com/ubuntu/ trusty main restricted
+deb http://archive.ubuntu.com/ubuntu/ trusty-updates main restricted
+deb-src http://archive.ubuntu.com/ubuntu/ trusty-updates main restricted
+deb http://archive.ubuntu.com/ubuntu/ trusty universe
+deb-src http://archive.ubuntu.com/ubuntu/ trusty universe
+deb http://archive.ubuntu.com/ubuntu/ trusty-updates universe
+deb-src http://archive.ubuntu.com/ubuntu/ trusty-updates universe
+deb http://archive.ubuntu.com/ubuntu/ trusty-security main restricted
+deb-src http://archive.ubuntu.com/ubuntu/ trusty-security main restricted
+deb http://archive.ubuntu.com/ubuntu/ trusty-security universe
+deb-src http://archive.ubuntu.com/ubuntu/ trusty-security universe"
 
 GCE="deb http://REPLACE.gce.clouds.archive.ubuntu.com/ubuntu/ trusty main restricted
 deb-src http://REPLACE.gce.clouds.archive.ubuntu.com/ubuntu/ trusty main restricted
