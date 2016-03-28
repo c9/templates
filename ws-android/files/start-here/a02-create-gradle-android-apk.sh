893d615cc99e6b194960f1309623c59f4825dc5b
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-28 09:36:27 -0700

    fixed broken link

--- ws-android/files/start-here/a02-create-gradle-android-apk.sh
+++ ws-android/files/start-here/a02-create-gradle-android-apk.sh
@@ -47,7 +47,7 @@ chmod a+x gradlew
 ./gradlew assembleDebug
 
 # Make the following a full path to where your index.html file is /home/ubuntu/workspace/www/index.html
-printf "\n\n<a href='$wow4/build/outputs/apk/$wow4-debug.apk'>$wow4/build/outputs/apk/$wow4-debug.apk</a><br><br>"  >> /home/ubuntu/workspace/www/index.html
+printf "\n\n<a href='../$wow4/build/outputs/apk/$wow4-debug.apk'>../$wow4/build/outputs/apk/$wow4-debug.apk</a><br><br>"  >> /home/ubuntu/workspace/www/index.html
 
 ls -l build/outputs/apk
 
