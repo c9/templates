11b665ccbfeced7d277d0a93e79ced262226e29a
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-30 23:17:22 -0700

    Update a01-create-ant-android-apk.sh

--- ws-android/files/start-here/a01-create-ant-android-apk.sh
+++ ws-android/files/start-here/a01-create-ant-android-apk.sh
@@ -49,10 +49,10 @@ ls -l
 
 echo "Look for you new android $wow4/bin $wow4-debug.apk"
 echo "right-click run index.html, then preview-preview running application to view webpage with .apk"
-
-
-echo "Or just click this link and open the web page"
 echo ""
+echo "For help click on https://github.com/hpssjellis/online-android-sdk-cloud9-help/tree/master/version-1.0.0-android-sdk-help/basic-operation-and-installation/a01-create-ant-android-apk-HELP.md"
+echo ""
+echo "Click this link to open your index.html web page"
 echo ""
 echo ""
 
