5c1d451464e30dd010af553df38f9d6000e24039
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-31 19:43:56 -0700

    Create live-update-WARNING.sh

---
+++ ws-android/files/start-here/live-update-WARNING.sh
@@ -0,0 +1,29 @@
+#!/bin/bash  
+
+# Find or copy this file to your cloud9 Android-SDK workspace
+# run it with the command
+# bash live-update-WARNING.sh
+
+#grabs the live update repository from github and merges the folder ubuntu files with your /home/ubuntu files including your workspace.
+
+
+echo "Warning. This bash program updates your Docker Cloud9 Workspace potential replacing it with any file in the github repository at"
+echo "https://github.com/hpssjellis/online-android-sdk-cloud9-live-update"
+echo ""
+echo "Please check the repository to make sure you want those files"
+echo "Proceed Y/n"
+read myAnswer
+if [ $myAnswer == 'y' ] || [ $myAnswer == 'Y' ]
+  then
+    echo "Installing live update files from github"
+    sudo git clone https://github.com/hpssjellis/online-android-sdk-cloud9-live-update.git /home/temp
+    rsync -avh --progress /home/temp/ubuntu /home
+    sudo rm -rf /home/temp
+
+  else
+    echo "Nothing new installed"
+fi
+  
+echo "Live update files in the repository at"
+echo "https://github.com/hpssjellis/online-android-sdk-cloud9-live-update"
+echo ""  
