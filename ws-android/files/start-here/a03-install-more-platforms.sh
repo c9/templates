9b3ace455e838c4496cb6bff1a2a68039b945998
Jeremy Ellis <keyfreemusic@gmail.com>
2016-04-03 17:10:33 -0700

    Update a03-install-more-platforms.sh

--- ws-android/files/start-here/a03-install-more-platforms.sh
+++ ws-android/files/start-here/a03-install-more-platforms.sh
@@ -26,6 +26,8 @@ android list sdk
 echo "------------------------------------------------------------------------"
 echo ""
 echo "Note: Installing the new build tools may break some links, so be careful"
+echo "Might be best with the build tools to wait for the newer version of the workspace"
+echo ""
 echo "Type the left hand side number of the SDK to install. example 1"
 
 read myNum
