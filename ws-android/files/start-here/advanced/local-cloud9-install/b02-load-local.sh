78787fcf25b8366c1bd3affcb461c9729acdabab
Jeremy Ellis <keyfreemusic@gmail.com>
2016-04-04 20:46:36 -0700

    Update b02-load-local.sh

--- ws-android/files/start-here/advanced/local-cloud9-install/b02-load-local.sh
+++ ws-android/files/start-here/advanced/local-cloud9-install/b02-load-local.sh
@@ -21,21 +21,8 @@ sudo ./scripts/install-sdk.sh
 # move pre-made apache2 config files to the correct location 
 # for Apache2 to run on port 8081 since cloud9 local is on port 8080
 
-sudo mv /home/ubuntu/start-here/advanced-local-install/ports.conf /etc/apache2/ports.conf
-sudo mv /home/ubuntu/start-here/advanced-local-install/001-cloud9.conf  /etc/apache2/sites-available/001-cloud9.conf
-
-
-./server.js -p 8080 -l 0.0.0.0 -a :
-  
-  
-#Edit /etc/apache2/ports.conf and change 8080 to 8081
-#Edit /etc/apache2/sites-available/001-cloud9.conf and change 8080 to 8081.
-
-sudo mv /home/ubuntu/start-here/advanced-local-install/ports.conf /etc/apache2/ports.conf
-sudo mv /home/ubuntu/start-here/advanced-local-install/001-cloud9.conf  /etc/apache2/sites-available/001-cloud9.conf
-
-
-#sudo service apache2 restart 
+sudo cp /home/ubuntu/start-here/advanced/local-cloud9-install/ports.conf /etc/apache2/ports.conf
+sudo cp /home/ubuntu/start-here/advanced/local-cloud9-install/001-cloud9.conf  /etc/apache2/sites-available/001-cloud9.conf
 
 
 
@@ -44,3 +31,10 @@ sudo mv /home/ubuntu/start-here/advanced-local-install/001-cloud9.conf  /etc/apa
 echo "For help click on "
 echo "$HELP_GITHUB$LATEST_VERSIONb02-load-local.sh.md"
 echo ""
+
+./server.js -p 8080 -l 0.0.0.0 -a :
+  
+
+
+
+
