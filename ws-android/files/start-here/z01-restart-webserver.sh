c3cfdb72f21ef14173c6f16ebdb3cc809a9ddc48
Jeremy Ellis <keyfreemusic@gmail.com>
2016-04-04 21:37:40 -0700

    Update z01-restart-webserver.sh

--- ws-android/files/start-here/z01-restart-webserver.sh
+++ ws-android/files/start-here/z01-restart-webserver.sh
@@ -6,6 +6,9 @@
 #  best in cloud 9 to just right click this file and select run
 
 
+echo "For help click on "
+echo "$HELP_GITHUB/z01-restart-webserver.sh.md"
+echo ""
 
 
 
@@ -14,9 +17,6 @@ echo "Activating apache2 webserver"
 service apache2 start
 
 
-echo "For help click on "
-echo "$HELP_GITHUB$LATEST_VERSIONz01-restart-webserver.sh.md"
-echo ""
 
 
 echo "Click this link and select open to view your web page"
